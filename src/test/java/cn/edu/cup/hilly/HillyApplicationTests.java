b9bbeb87b2bd3339113df882fca76b3713a1cb75
WangHH51322 <1252011170@qq.com>
2021-08-05 15:58:03 +0800

    杨起更新前备份

--- src/test/java/cn/edu/cup/hilly/HillyApplicationTests.java
+++ src/test/java/cn/edu/cup/hilly/HillyApplicationTests.java
@@ -7,6 +7,7 @@ import cn.edu.cup.hilly.dataSource.service.FileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.security.provider.MD5;
 
 @SpringBootTest
 class HillyApplicationTests {
